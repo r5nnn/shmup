d8760f8889065aad5a8521991cda8d7a9533322e
r5ne <renetimantsev@gmail.com>
2024-01-21 21:58:48 +0000

    added player hitbox added movement added border detection added window icon and name capped fps at 60

--- main.py
+++ main.py
@@ -4,20 +4,39 @@ import pygame
 pygame.init()
 
 # window parameters
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 800
+SCREEN_WIDTH, SCREEN_HEIGHT = 600, 800
 
 # initialise game assets
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 hitbox = pygame.Rect((300, 400, 10, 10))
+pygame.display.set_caption('shmup alpha 0.0.0')
+icon = pygame.image.load('assets\icon.png')
+pygame.display.set_icon(icon)
+clock = pygame.time.Clock()
 
 # start gameloop
 run = True
 while run:
+    # update screen
+    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, (255, 0, 0), hitbox)
+    # key & screen border detection
+    key = pygame.key.get_pressed()
+    if hitbox.top > 0 and key[pygame.K_w]:
+        hitbox.move_ip(0, -4)
+    if hitbox.left > 0 and key[pygame.K_a]:
+        hitbox.move_ip(-4, 0)
+    if hitbox.bottom < SCREEN_HEIGHT and key[pygame.K_s]:
+        hitbox.move_ip(0, 4)
+    if hitbox.right < SCREEN_WIDTH and key[pygame.K_d]:
+        hitbox.move_ip(4, 0)
     for event in pygame.event.get():
         # exit out method
         if event.type == pygame.QUIT:
             run = False
+    # update the game every 60 ticks (caps game at 60fps)
+    pygame.display.flip()
+    clock.tick(60)
 
 # game interrupt
-pygame.QUIT()
+pygame.quit()
