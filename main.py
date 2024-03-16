bacb2b81d2a848d25171378d55c8e1f73c6960e1
r5ne <renetimantsev@gmail.com>
2024-03-16 19:43:21 +0000

    huge update huge amount of stuff ngl I forgot what I added

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import sys
 from ctypes import windll
 
 # Initialise game assets and variables
-
+pygame.mixer.pre_init(75100, -16, 2, 2048)  # (frequency, size, channels, buffer)
 pygame.init()  # all pygame modules initialised - not all will be used, but it is simpler this way
 
 # window dimensions
 screenx = windll.user32.GetSystemMetrics(0)
 screeny = windll.user32.GetSystemMetrics(1)
-winx = 600
-winy = 800
+winx = screenx
+winy = screeny
 screen = pygame.display.set_mode([winx, winy], pygame.SCALED, vsync=0)  # creates the window
 
 # useful shorthands and global variables
@@ -28,16 +28,25 @@ QUATERNARY = (85, 85, 85)  # light gray
 ACCENT = (255, 148, 252)  # light pink
 
 # assets
-FONT = 'assets\\fonts\\Raleway.ttf'  # font taken from: https://fonts.google.com/specimen/Raleway
+
+# fonts
+FONT = 'assets\\fonts\\editundo.ttf'  # font taken from: https://www.dafont.com/edit-undo.font
+
+# textures
 PLAYER = pygame.image.load('assets\\textures\\player.png').convert()
 ICON = pygame.image.load('assets\\textures\\icon.png').convert()
-backdrop = pygame.image.load('assets\\textures\\STAGE.png').convert()  # surface to blit everything to
+LOGO = pygame.image.load('assets\\textures\\logo.png').convert()
+BACKDROP = pygame.image.load('assets\\textures\\menu.png').convert()  # surface to blit everything to
 # from https://www.pygame.org/docs/ref/surface.html#pygame.Surface.blit
 # blit means to Draw a source Surface onto the current Surface. The draw can be positioned with the dest argument.
 
+# music
+MENULOOP = pygame.mixer.Sound('assets\\music\\menuloop.wav')
+MENULOOP.set_volume(0.2)
+
 # game info
-STAGE_STRUCTURE = ['home', 'paused', 'options', 'keybinds']
-KEYS = [['ESC', 'Return to previous screen']]
+STAGE_STRUCTURE = ['home', 'options', 'keybinds']
+KEYS = [['ESC', '''Pauses the game/enters options when in main menu also acts as a back key when in any other menu''']]
 KEY_ARRAY = [[50, 270 + (i * 40), KEYS[i][0], 32, FONT, WHITE] for i in range(len(KEYS))]
 INFO_ARRAY = [[winx - 50, 270 + (i * 40), KEYS[i][1], 32, FONT, WHITE] for i in range(len(KEYS))]
 
@@ -51,7 +60,7 @@ pygame.display.set_caption('shmup alpha 0.0.1')
 pygame.display.set_icon(ICON)
 
 # window background
-backdrop_coords = screen.get_rect()  # coordinates of surface
+BACKDROP_coords = screen.get_rect()  # coordinates of surface
 
 # game speed
 fps = 165
@@ -77,29 +86,35 @@ class Txt:
         self.y = y
         self.x = x
         self.font = pygame.font.Font(font, size)  # creates a font object
-        self.text = self.font.render(text, True, self.text_color)  # create a surface with the specified
-        # text drawn on it
-        if self.center is not None:
-            self.text_rect = self.text.get_rect(center=self.center)  # create a temporary rect the size of the text and
-            # set the center to tuple given
-            self.text_rect.center = self.center
-        else:
-            match self.pos:
-                case 'left':
-                    self.text_rect = self.text.get_rect()  # create a temporary rect the size of the text and specify
-                    # the x and y coordinates (since it defaults to 0)
-                    self.text_rect.topleft = (self.x, self.y)
-                    self.center = self.text_rect.center
-                case 'right':
-                    self.text_rect = self.text.get_rect()
-                    self.text_rect.topright = (self.x, self.y)
-                    self.center = self.text_rect.center
+        self.text_split = text.split('\n')
+        self.text_list = []
+        for sentence in self.text_split:
+            self.fontRender = self.font.render(sentence, True, self.text_color)  # create a surface with the specified
+            # text drawn on it
+            self.text_list.append(self.fontRender)
+        for lines in self.text_list:
+            if self.center is not None:
+                self.text_rect = lines.get_rect(center=self.center)  # create a temporary rect the size of the text and
+                # set the center to tuple given
+                self.text_rect.center = self.center
+            else:
+                match self.pos:
+                    case 'left':
+                        self.text_rect = lines.get_rect()  # create a temporary rect the size of the text and specify
+                        # the x and y coordinates (since it defaults to 0)
+                        self.text_rect.topleft = (self.x, self.y)
+                        self.center = self.text_rect.center
+                    case 'right':
+                        self.text_rect = lines.get_rect()
+                        self.text_rect.topright = (self.x, self.y)
+                        self.center = self.text_rect.center
 
     def create(self):
         """
         call this method to display the button on a surface
         """
-        screen.blit(self.text, self.text_rect)
+        for lines in self.text_list:
+            screen.blit(lines, self.text_rect)
 
 
 class Btn:
@@ -178,15 +193,32 @@ class Btn:
             self.text_color = self.text_released
 
 
-class Player:
-    def __init__(self, x, y, width, height, speed, img):
-        self.img = img
+class Img:
+    def __init__(self, x, y, width, height, img, center=None):
+        self.center = center
         self.width = width
+        self.y = y
+        self.x = x
+        self.img = img
         self.height = height
         self.rect = pygame.rect.Rect(x, y, self.width, self.height)
+        if self.center is not None:
+            self.rect.center = self.center
+        else:
+            self.center = self.rect.center
         self.rect_surf = pygame.Surface(self.rect.size, pygame.SRCALPHA)
-        self.speed = speed
         self.bounds = None
+        self.border = None
+
+    def create(self, *args):
+        pygame.draw.rect(self.rect_surf, ACCENT, self.rect_surf.get_rect(center=self.center))
+        screen.blit(self.img, self.rect)
+
+
+class Player(Img):
+    def __init__(self, x, y, width, height, img, speed):
+        super().__init__(x, y, width, height, img)
+        self.speed = speed
 
     def create(self, bounds):
         border = bounds.get_rect()
@@ -209,38 +241,65 @@ def cmd(name):
     call this method to update the stage of the screen
     :param name: the stage you want to switch to
     """
-    global STAGE, STAGE_STRUCTURE
-    if name == 'back':
-        STAGE_STRUCTURE_POINTER = STAGE_STRUCTURE.index(STAGE)
-        if INGAME:
-            STAGE_STRUCTURE[0] = 'game'
-            if STAGE_STRUCTURE_POINTER != 0:
+    global STAGE, STAGE_STRUCTURE, main
+    match name:
+        case 'back':
+
+            if INGAME:
+                STAGE_STRUCTURE.insert(1, 'paused')
                 STAGE_STRUCTURE_POINTER = STAGE_STRUCTURE.index(STAGE)
-                STAGE = STAGE_STRUCTURE[STAGE_STRUCTURE_POINTER - 1]
+                STAGE_STRUCTURE[0] = 'game'
+                if STAGE_STRUCTURE_POINTER != 0:
+                    STAGE_STRUCTURE_POINTER = STAGE_STRUCTURE.index(STAGE)
+                    STAGE = STAGE_STRUCTURE[STAGE_STRUCTURE_POINTER - 1]
+                else:
+                    STAGE = STAGE_STRUCTURE[STAGE_STRUCTURE_POINTER]
             else:
-                STAGE = STAGE_STRUCTURE[STAGE_STRUCTURE_POINTER]
-        else:
-            STAGE_STRUCTURE[0] = 'home'
-            if STAGE_STRUCTURE_POINTER != 0:
-                STAGE = STAGE_STRUCTURE[STAGE_STRUCTURE_POINTER - 1]
+                STAGE_STRUCTURE_POINTER = STAGE_STRUCTURE.index(STAGE)
+                if 'paused' in STAGE_STRUCTURE:
+                    STAGE_STRUCTURE.remove('paused')
+                STAGE_STRUCTURE[0] = 'home'
+                if STAGE_STRUCTURE_POINTER != 0:
+                    STAGE = STAGE_STRUCTURE[STAGE_STRUCTURE_POINTER - 1]
+                else:
+                    STAGE = STAGE_STRUCTURE[STAGE_STRUCTURE_POINTER]
+        case 'quit':
+            pygame.quit()
+            try:
+                sys.exit()
+            finally:
+                main = False
+        case _:
+            STAGE = name
+
+
+def generate(arr):
+    for list1 in arr:
+        for listObject in list1:
+            if arr.index(list1) == 0:
+                listObject.create(screen, events)
             else:
-                STAGE = STAGE_STRUCTURE[STAGE_STRUCTURE_POINTER]
-    else:
-        STAGE = name
+                # noinspection
+                listObject.create()
 
 
 # buttons
-back_btn = Btn(0, 0, 205, 50, 30, TERTIARY, QUATERNARY, SECONDARY, PRIMARY, 'Back',
+back_btn = Btn(0, 0, 205, 50, 36, TERTIARY, QUATERNARY, SECONDARY, PRIMARY, 'Back',
                lambda b: cmd('back'), center=(winx / 2, 300))
-options_btn = Btn(0, 0, 205, 50, 30, TERTIARY, QUATERNARY, SECONDARY, PRIMARY, 'Options',
+options_btn = Btn(0, 0, 205, 50, 36, TERTIARY, QUATERNARY, SECONDARY, PRIMARY, 'Options',
                   lambda b: cmd('options'), center=(winx / 2, 225))
-keybinds_btn = Btn(0, 0, 205, 50, 30, TERTIARY, QUATERNARY, SECONDARY, PRIMARY, 'Keybinds',
+keybinds_btn = Btn(0, 0, 205, 50, 36, TERTIARY, QUATERNARY, SECONDARY, PRIMARY, 'Keybinds',
                    lambda b: cmd('keybinds'), center=(winx / 2, 225))
-play_btn = Btn(0, 0, 300, 75, 35, TERTIARY, QUATERNARY, SECONDARY, PRIMARY, 'Play',
+play_btn = Btn(0, 0, 300, 75, 64, TERTIARY, QUATERNARY, SECONDARY, PRIMARY, 'Play',
                lambda b: cmd('game'), center=(winx / 2, winy / 2))
+exit_btn = Btn(0, 0, 205, 50, 36, TERTIARY, QUATERNARY, SECONDARY, PRIMARY, 'Exit Game',
+               lambda b: cmd('home'), center=(winx / 2, 370))
+options1_btn = Btn(0, 0, 300, 75, 64, TERTIARY, QUATERNARY, SECONDARY, PRIMARY, 'Options',
+                   lambda b: cmd('options'), center=(winx / 2, winy / 2 + 100))
+quit_btn = Btn(0, 0, 300, 75, 64, TERTIARY, QUATERNARY, SECONDARY, PRIMARY, 'Quit',
+               lambda b: cmd('quit'), center=(winx / 2, winy / 2 + 200))
 
 # display text
-temp_txt = Txt(0, 0, 'Press esc to pause', 32, FONT, WHITE, center=(winx / 2, winy / 2 - 100))
 paused_txt = Txt(0, 0, 'Paused', 64, FONT, WHITE, center=(winx / 2, 150))
 options_txt = Txt(0, 0, 'Options', 64, FONT, WHITE, center=(winx / 2, 150))
 keybinds_txt = Txt(0, 0, 'Keybinds', 64, FONT, WHITE, center=(winx / 2, 150))
@@ -249,11 +308,20 @@ keybindsInfo_txt = Txt(winx - 50, 200, 'Description', 48, FONT, WHITE, pos='righ
 KEY_ARRAY = [Txt(i[0], i[1], i[2], i[3], i[4], i[5]) for i in KEY_ARRAY]
 INFO_ARRAY = [Txt(i[0], i[1], i[2], i[3], i[4], i[5], pos='right') for i in INFO_ARRAY]
 
+# images
+logo_img = Img(0, 0, 900, 124, LOGO, center=(winx / 2, winy / 2 - 200))
+
+# 2d array of objects for each screen for easy initialising with for loop
+# first list contains buttons, while second list contains text
+home = [[play_btn, options1_btn, quit_btn], [logo_img]]
+paused = [[back_btn, exit_btn, options_btn], [paused_txt]]
+options = [[keybinds_btn, back_btn], [options_txt]]
+keybinds = [[], [keybinds_txt, keybindsKEY_ARRAY_txt, keybindsInfo_txt]]
+
 # game objects
-player = Player(winx/2, winy/2, 27, 19, 2, PLAYER)
+player = Player(winx / 2, winy / 2, 27, 19, PLAYER, 2)
 
 windll.user32.SetCursorPos(screenx // 2, screeny // 2)
-
 # Game Mainloop
 
 while RUN:
@@ -267,36 +335,28 @@ while RUN:
                 main = False
 
         if event.type == pygame.KEYDOWN:  # alternative way to exit through keybind
-            if event.key == pygame.K_q:
-                pygame.quit()
-                try:
-                    sys.exit()
-                finally:
-                    main = False
             if event.key == pygame.K_ESCAPE:
-                if STAGE == 'home' or STAGE == 'game':
-                    STAGE = 'paused'
-                else:
-                    cmd('back')
-    screen.blit(backdrop, backdrop_coords)
-
+                match STAGE:
+                    case 'home':
+                        cmd('options')
+                    case 'game':
+                        cmd('paused')
+                    case _:
+                        cmd('back')
+    screen.blit(BACKDROP, BACKDROP_coords)
     # match case statement to toggle between multiple stages when navigating menus and options
     match STAGE:
         case 'home':
-            temp_txt.create()
-            play_btn.create(screen, events)
+            BACKDROP = pygame.image.load('assets\\textures\\menu.png').convert()
+            if not pygame.mixer.get_busy():
+                MENULOOP.play(loops=-1, fade_ms=1000)
+            generate(home)
         case 'paused':
-            paused_txt.create()
-            options_btn.create(screen, events)
-            back_btn.create(screen, events)
+            generate(paused)
         case 'options':
-            options_txt.create()
-            keybinds_btn.create(screen, events)
-            back_btn.create(screen, events)
+            generate(options)
         case 'keybinds':
-            keybinds_txt.create()
-            keybindsKEY_ARRAY_txt.create()
-            keybindsInfo_txt.create()
+            generate(keybinds)
             for i in KEY_ARRAY:
                 i.create()
             for i in INFO_ARRAY:
@@ -304,7 +364,8 @@ while RUN:
         case 'game':
             INGAME = True
             player.create(screen)
+            BACKDROP = pygame.image.load('assets\\textures\\stage.png').convert()
 
     mouse = pygame.mouse.get_pos()  # stores the (x,y) coordinates into the variable as a tuple
-    pygame.display.flip()  # update the backdrop surface onto the window
+    pygame.display.flip()  # update the BACKDROP surface onto the window
     clock.tick(fps)  # update the clock with the delay of the refresh rate
