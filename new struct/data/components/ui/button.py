72ec37f0e42e15dced3040762fba40873b67515f
r5ne <renetimantsev@gmail.com>
2024-09-22 17:24:14 +0100

    refactored button.py to make code more readable, implemented better image buttons.

--- new struct/data/components/ui/button.py
+++ new struct/data/components/ui/button.py
@@ -1,21 +1,15 @@
 from typing import Callable
 
 import pygame.display
+from pygame.pixelcopy import surface_to_array
 
-from . import RectUpdateNeeded
+from . import RectUpdateNeeded, widgethandler
 from .text import Text
 from ..events import Mouse
 from data.utils import CustomTypes
 
 
-class Button:
-    coords = RectUpdateNeeded()
-    align = RectUpdateNeeded()
-    button_colors = {'inactive': pygame.Color(30, 30, 30),
-                     'hovered': pygame.Color(35, 35, 35),
-                     'clicked': pygame.Color(85, 85, 85)
-                     }
-
+class ButtonBase:
     def __init__(self,
                  surface: pygame.Surface,
                  rect: pygame.Rect,
@@ -26,44 +20,12 @@ class Button:
                  border_thickness: int = 0,
                  on_click: Callable = None,
                  on_release: Callable = None,
-                 text: str = "",
-                 text_align: tuple[str, str] = (),
-                 text_colors: dict[str, tuple] = None,
-                 font: pygame.font.Font = None,
-                 font_size: int = 32,
-                 image: pygame.Surface = None,
-                 image_align: tuple[str, str] = (),
-                 margin: int = 20,
                  radius: int = 0):
         """
-        Class for creating and updating buttons.
-        
-        Button consists of a background rect, optional border and text or image
-        surface.
-        Allows for customising the button and text color when clicked and 
-        hovered as well as calling functions on click and on release.
+        Base class for button functionality.
 
-        :param surface: Surface which the text surface will be blit onto.
-        :param align: Alignment of the button background rect coordinates.
-        :param colors: Dictionary of colors that the button background rect will
-        cycle through upon clicking or hovering the button.
-        :param border_colors: Dictionary of colors that the button border rect
-        will cycle through upon clicking or hovering the button.
-        :param border_thickness: Thickness of the border around the button
-        background rect.
-        :param on_click: Function to call when clicking the button.
-        :param on_release: Function to call when releasing the button after
-        clicking.
-        :param text: Text to display on the button.
-        :param text_align: Alignment of the text in the button.
-        :param text_colors: Dictionary of colors that the button text will
-        cycle through upon clicking or hovering the button.
-        :param font: Font of the text to display.
-        :param image: Image to display on the button
-        :param image_align: Alignment of the image in the button.
-        :param margin: Margin to use when aligning text or images on the button.
-        :param radius: The amount of curve the button background rect should
-        have on the corners.
+        Handles the basic appearance and interaction such as border, colors,
+        and on-click/on-release events. Child classes will add text or image-specific logic.
         """
         self._surface = surface
         self._rect = rect
@@ -72,35 +34,31 @@ class Button:
         self._align = align
         self._align_rect(self._rect, self._align, self._rect.topleft)
         self._coords = getattr(self._rect, self._align)
-        self.colors = colors if colors is not None else Button.button_colors
-        self._color = self.colors['inactive']
+
+        # Color management
+        self.colors = colors if colors is not None else None
+        self._color = self.colors[
+            'inactive'] if self.colors is not None else None
+
+        # Border management
         self.border = border
-        self.border_colors = border_colors if border_colors is not None \
-            else Button.button_colors
-        self._border_color = self.border_colors['inactive']
+        self.border_colors = border_colors if border_colors is not None else None
+        self._border_color = self.border_colors[
+            'inactive'] if self.border_colors else None
         self._border_thickness = border_thickness
-        # no need to align border rect as button rect already aligned.
         self._border_rect = pygame.Rect(
             self._rect.left + self._border_thickness,
             self._rect.top + self._border_thickness,
             self._width - self._border_thickness * 2,
-            self._height - self._border_thickness * 2)
-        self.on_click = on_click if on_click is not None else lambda *args: None
-        self.on_release = on_release if on_release is not None else \
-            lambda *args: None
-        self.text_colors = text_colors if text_colors is not None else \
-            {'inactive': (255, 255, 255),
-             'hovered': (255, 255, 255),
-             'clicked': (255, 255, 255)}
-        self._text_color = self.text_colors['inactive']
-        self._text = Text(self._surface, text, (0, 0), font_size, font,
-                          color=self._text_color)
-        self.text_align = text_align
-        self.image = image if image is not None else None
-        self.image_align = image_align
-        self.margin = margin
-        self.radius = radius
+            self._height - self._border_thickness * 2
+        )
 
+        # Event handlers
+        self.on_click = on_click if on_click is not None else lambda *args: None
+        self.on_release = on_release if on_release is not None else lambda \
+            *args: None
+
+        self.radius = radius
         self.clicked = False
         self._disabled = False
 
@@ -111,7 +69,6 @@ class Button:
     @surface.setter
     def surface(self, value):
         self._surface = value
-        self._text.surface = value
 
     @property
     def width(self):
@@ -157,68 +114,19 @@ class Button:
         self._border_rect.topleft = (self._rect.left + self._border_thickness,
                                      self._rect.top + self._border_thickness)
 
-    @property
-    def text_align(self):
-        return self._text_align
-
-    @text_align.setter
-    def text_align(self, value):
-        self._text_align = value
-        self._align_text(self._text) if self._text is not None else None
-
-    @property
-    def image_align(self):
-        return self._image_align
-
-    @image_align.setter
-    def image_align(self, value):
-        self._image_align = value
-        self._align_image(self.image) if self.image is not None else None
-
     def contains(self, x, y):
+        """Basic collision detection for the button rectangle."""
         return (self._rect.left < x - self._surface.get_abs_offset()[0]
                 < self._rect.left + self._width) and \
-               (self._rect.top < y - self._surface.get_abs_offset()[1]
-                < self._rect.top + self._height)
+            (self._rect.top < y - self._surface.get_abs_offset()[1]
+             < self._rect.top + self._height)
 
     def _align_rect(self, rect, align, coords):
-        self._requires_rect_update = False
         setattr(rect, align, coords)
-
-    def _align_text(self, surface):
-        self.text_rect = surface.rect
-        self.text_rect.center = self._rect.center
-
-        if len(self.text_align) == 2:
-            if self.text_align[0] == 'left':
-                self.text_rect.left = self.x + self.margin
-            elif self.text_align[0] == 'right':
-                self.text_rect.right = self.x + self.width - self.margin
-
-            if self.text_align[1] == 'top':
-                self.text_rect.top = self.y + self.margin
-            elif self.text_align[1] == 'bottom':
-                self.text_rect.bottom = self.y + self.height - self.margin
-
-        surface.coords = self.text_rect.left, self.text_rect.top
-
-    def _align_image(self, surface):
-        self._image_rect = surface.get_rect()
-        self._image_rect.center = (self.x + self.width // 2, self.y +
-                                   self.height // 2)
-
-        if len(self.image_align) == 2:
-            if self.image_align[0] == 'left':
-                self._image_rect.left = self.x + self.margin
-            elif self.image_align[0] == 'right':
-                self._image_rect.right = self.x + self.width - self.margin
-
-            if self.image_align[1] == 'top':
-                self._image_rect.top = self.y + self.margin
-            elif self._image_align[1] == 'bottom':
-                self._image_rect.bottom = self.y + self.height - self.margin
+        self._coords = self._x, self._y = getattr(rect, align)
 
     def update(self):
+        """Updates the button based on mouse state."""
         if not self._disabled:
             mouse_state = Mouse.get_mouse_state()
             x, y = Mouse.get_mouse_pos()
@@ -230,34 +138,202 @@ class Button:
                 elif mouse_state == mouse_state.CLICK:
                     self.clicked = True
                     self.on_click()
-                    self._color = self.colors['clicked']
-                    self._text_color = self.text_colors['clicked']
-                    self._border_color = self.border_colors.get('clicked')
+                    if self.colors is not None:
+                        self._color = self.colors['clicked']
+                    if self.border_colors is not None:
+                        self._border_color = self.border_colors.get('clicked')
 
-                elif mouse_state == mouse_state.DRAG and self.clicked:
-                    self._color = self.colors['clicked']
-                    self._border_color = self.border_colors['clicked']
+                elif mouse_state in {mouse_state.DRAG,
+                                     mouse_state.HOVER} and self.clicked:
+                    if self.colors is not None:
+                        self._color = self.colors['clicked']
+                    if self.border_colors is not None:
+                        self._border_color = self.border_colors.get('clicked')
 
-                elif (mouse_state == mouse_state.HOVER or
-                      mouse_state == mouse_state.DRAG):
-                    self._color = self.colors['hovered']
-                    self._border_color = self.border_colors.get('hovered')
+                elif mouse_state == mouse_state.HOVER:
+                    if self.colors is not None:
+                        self._color = self.colors['hovered']
+                    if self.border_colors is not None:
+                        self._border_color = self.border_colors.get('hovered')
 
             else:
                 self.clicked = False
-                self._color = self.colors['inactive']
-                self._border_color = self.border_colors.get('inactive')
+                if self.colors is not None:
+                    self._color = self.colors['inactive']
+                if self.border_colors is not None:
+                    self._border_color = self.border_colors.get('inactive')
 
     def blit(self):
-        if self._requires_rect_update:
-            self._align_rect(self._rect, self._align, self._coords)
+        """Draws the button onto the surface."""
         if self.border:
             pygame.draw.rect(
                 self._surface, self.border_color, self._border_rect,
-                border_radius=self.radius)
-        pygame.draw.rect(
-            self._surface, self.color, self._rect,
-            border_radius=self.radius)
-        if self.image is not None:
-            self._surface.blit(self.image, self._image_rect)
-        self._text.blit() if self._text is not None else None
+                border_radius=self.radius
+            )
+        if self.colors is not None:
+            pygame.draw.rect(
+                self._surface, self.color, self._rect,
+                border_radius=self.radius
+            )
+
+
+class ButtonText(ButtonBase):
+    def __init__(self,
+                 surface: pygame.Surface,
+                 rect: pygame.Rect,
+                 text: str = "",
+                 text_colors: dict[str, tuple] = None,
+                 font: pygame.font.Font = None,
+                 font_size: int = 32,
+                 text_align: tuple[str, str] = (),
+                 margin: int = 20,
+                 **kwargs):
+        """
+        Button class that includes text rendering and color management.
+        """
+        super().__init__(surface, rect, **kwargs)
+
+        self.text_colors = text_colors if text_colors is not None else \
+            {'inactive': (255, 255, 255),
+             'hovered': (255, 255, 255),
+             'clicked': (255, 255, 255)}
+        self._text_color = self.text_colors['inactive']
+
+        # Text instance
+        self._text = Text(self._surface, text, (0, 0), font_size, font,
+                          color=self._text_color)
+        self.text_align = text_align
+        self.margin = margin
+
+        # Align the text inside the button's rect
+        self._align_text(self._text)
+
+    @property
+    def surface(self):
+        return self._surface
+
+    @surface.setter
+    def surface(self, value):
+        self._surface = value
+        self._text.surface = value
+
+    @property
+    def text_align(self):
+        return self._text_align
+
+    @text_align.setter
+    def text_align(self, value):
+        self._text_align = value
+        self._align_text(self._text) if self._text is not None else None
+
+    def _align_text(self, surface):
+        """Aligns the text relative to the button rect."""
+        self.text_rect = surface.rect
+        self.text_rect.center = self._rect.center
+
+        if len(self.text_align) == 2:
+            if self.text_align[0] == 'left':
+                self.text_rect.left = self._rect.left + self.margin
+            elif self.text_align[0] == 'right':
+                self.text_rect.right = self._rect.right - self.margin
+
+            if self.text_align[1] == 'top':
+                self.text_rect.top = self._rect.top + self.margin
+            elif self.text_align[1] == 'bottom':
+                self.text_rect.bottom = self._rect.bottom - self.margin
+
+        surface.coords = self.text_rect.left, self.text_rect.top
+
+    def blit(self):
+        """Draw the button and its text onto the surface."""
+        super().blit()
+        self._text.blit()
+
+    def update(self):
+        """Updates the button and the text color based on mouse state."""
+        super().update()
+        x, y = Mouse.get_mouse_pos()
+        if self.contains(x, y):
+            self._text.color = self.text_colors[
+                'hovered'] if not self.clicked else self.text_colors['clicked']
+        else:
+            self._text.color = self.text_colors['inactive']
+        self._text.update()
+
+
+class ButtonImage(ButtonBase):
+    def __init__(self,
+                 surface: pygame.Surface,
+                 rect: pygame.Rect,
+                 images: tuple,
+                 use_rect_collision: bool = False,
+                 image_align: tuple[str, str] = (),
+                 **kwargs):
+        """
+        Button class that includes image rendering and allows for toggling
+        between rect-based and pixel-perfect collision detection.
+        """
+        super().__init__(surface, rect, **kwargs)
+
+        self.images = images
+        self._current_image = self.images[0]
+        self.use_rect_collision = use_rect_collision
+        self.image_align = image_align
+
+        self._align_image()
+
+    @property
+    def image_align(self):
+        return self._image_align
+
+    @image_align.setter
+    def image_align(self, value):
+        self._image_align = value
+        self._align_image()
+
+    def _align_image(self):
+        """Aligns the image relative to the button's rect."""
+        self.image_rect = self._current_image.get_rect()
+        self.image_rect.center = self._rect.center
+
+        if len(self.image_align) == 2:
+            if self.image_align[0] == 'left':
+                self.image_rect.left = self._rect.left
+            elif self.image_align[0] == 'right':
+                self.image_rect.right = self._rect.right
+
+            if self.image_align[1] == 'top':
+                self.image_rect.top = self._rect.top
+            elif self.image_align[1] == 'bottom':
+                self.image_rect.bottom = self._rect.bottom
+
+    def contains(self, x, y):
+        """Check if the mouse is over the image or rect, depending on collision mode."""
+        if self.use_rect_collision:
+            return super().contains(x, y)  # Use rect-based collision
+        else:
+            local_x = x - self.image_rect.left
+            local_y = y - self.image_rect.top
+            try:
+                return self._current_image.get_at((local_x, local_y)).a != 0  # Pixel-perfect collision
+            except IndexError:
+                return False
+
+    def update(self):
+        """Updates the button based on mouse state."""
+        super().update()
+        x, y = Mouse.get_mouse_pos()
+        if self.contains(x, y):
+            if not self.clicked:
+                self._current_image = self.images[1]  # Hover
+            else:
+                self._current_image = self.images[2]  # Clicked
+        else:
+            self._current_image = self.images[0]  # Idle
+
+    def blit(self):
+        """Draw the image onto the surface. Optionally draw the button rect if using rect collision."""
+        if self.use_rect_collision:
+            pygame.draw.rect(self._surface, self._color, self._rect, border_radius=self.radius)
+
+        self._surface.blit(self._current_image, self.image_rect.topleft)
--- new struct/data/states/title.py
+++ new struct/data/states/title.py
@@ -2,7 +2,7 @@ import pygame
 
 from . import State
 from ..components.ui import widgethandler
-from ..components.ui.button import Button
+from ..components.ui.button import ButtonBase, ButtonImage, ButtonText
 from data.utils import fonts, graphics
 
 
@@ -14,11 +14,14 @@ class Title(State):
         dimensions = (self._screen_size[0]*0.98-self.logo.get_width(),
                       self._screen_size[1]*0.1)
         self.surfaces.append([self.logo, dimensions])
-        dimensions = (self._screen_size[0]*0.98-300,
-                      self._screen_size[1]*0.1 + self.logo.get_height())
-        self.button = Button(self.screen, pygame.Rect(*dimensions, 300, 100),
-                             text='Play', radius=10, font=fonts('editundo'))
+        dimensions = (self._screen_size[0]*0.98-self.logo.get_width()/2-100/2,
+                      self._screen_size[1]*0.1 + self.logo.get_height()*1.1)
+        self.button = ButtonImage(
+            self.screen, pygame.Rect(*dimensions, 100, 100),
+            tuple(pygame.transform.scale_by(i, 3)
+                  for i in graphics('play').values()))
         widgethandler.WidgetHandler.add_widget(self.button)
+        widgethandler.WidgetHandler.move_to_bottom(self.button)
 
     def update_screen(self):
         widgethandler.WidgetHandler.update_screen(pygame.display.get_surface())
