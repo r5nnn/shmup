05543f419c2bbcff79dd3b90d996c577cc59e701
r5ne <renetimantsev@gmail.com>
2024-09-24 21:55:54 +0100

    Minor changes to input.py

--- new struct/data/components/input.py
+++ new struct/data/components/input.py
@@ -1,6 +1,8 @@
+from typing import Callable
+
 import pygame
 
-from . import Singleton
+from data.utils import Singleton
 
 class InputManager(metaclass=Singleton):
     """Class that stores all the input states"""
@@ -13,7 +15,7 @@ class InputManager(metaclass=Singleton):
         self._mouse_buttons = list()
         self._mouse_pos = (0, 0)
    
-    def process_events(self, events: pygame.Eventlist):
+    def process_events(self, events: list):
         """Clears keydown and keyup events, iterates through
         pygame events and updates the input lists as necessary.
         
@@ -36,23 +38,23 @@ class InputManager(metaclass=Singleton):
 
         self._mouse_pos = pygame.mouse.get_pos()
 
-    def is_key_pressed(self, key):
+    def is_key_pressed(self, key: int):
         """returns: True if key is in the held_keys list."""
         return key in self._held_keys
 
-    def is_key_down(self, key):
+    def is_key_down(self, key: int):
         """returns: True if key is in the keydown_events list."""
         return key in self._keydown_events
 
-    def is_key_up(self, key):
+    def is_key_up(self, key: int):
         """returns: True if key is in the keyup_events list."""
         return key in self._keyup_events
 
-    def is_mouse_button_pressed(self, button):
+    def is_mouse_button_pressed(self, button: int) -> bool:
         """returns: True if button is in the mouse_buttons list."""
         return button in self._mouse_buttons
 
-    def get_mouse_pos(self):
+    def get_mouse_pos(self) -> tuple[int, int]:
         """returns: The position of the mouse."""
         return self._mouse_pos
 
@@ -67,18 +69,20 @@ class InputBinder:
         """Bind a specific input combination to an action."""
         self._bindings[inputs] = action
 
-    def process_bindings(self, input_manager):
+    def process_bindings(self, input_manager: InputManager):
         """Check current input state and trigger actions."""
         for inputs, action in self._bindings.items():
             if self._are_inputs_active(inputs, input_manager):
                 action()
 
-    def _are_inputs_active(self, inputs, input_manager):
+    @staticmethod
+    def _are_inputs_active(inputs, input_manager):
+        print(type(inputs), "inputs")
         """Check if the required inputs are active based on the InputManager."""
         for input_type, value in inputs:
             if input_type == 'key' and not input_manager.is_key_pressed(value):
                 return False
-            elif input_type == 'mouse' and not input_manager.is_mouse_button_pressed(value):
+            elif (input_type == 'mouse' and not
+            input_manager.is_mouse_button_pressed(value)):
                 return False
         return True
-  
