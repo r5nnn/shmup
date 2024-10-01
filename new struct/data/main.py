6482ffb193708f3e93bb6eee75a1b0776e214c2e
r5ne (well behaved) <147042887+r5nnn@users.noreply.github.com>
2024-10-01 10:06:31 +0100

    Added docstrings

--- new struct/data/main.py
+++ new struct/data/main.py
@@ -1,8 +1,14 @@
+"""Submodule that starts and runs the game.
+
+Does not contain logic, this submodule only starts and facilitates running the game.
+Declares the state dictionary with every state in the game, initialises the 
+Control class."""
 from . import control
 from .states import title, options
 
 
 def main():
+    """Runs the game."""
     states = {
         "title": title.Title,
         "options": options.Options
