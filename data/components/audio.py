459c278c463236cc0476a74bca3cc907a70b6758
r5ne (well behaved) <147042887+r5nnn@users.noreply.github.com>
2024-10-02 12:05:20 +0100

    added docstrings in correct style

--- data/components/audio.py
+++ data/components/audio.py
@@ -1,3 +1,4 @@
+"""Module for initiating and handling the playback of audio from audio files."""
 import warnings
 
 import pygame
@@ -8,26 +9,11 @@ from data.core.prepare import audio_paths
 
 
 class Audio:
-    _channel_counter = 0  # Class-level counter to assign unique channels
+    """Class for creating and managing pygame channel and sound objects.
+    Keeps track of channels used, assigns new channel each time class is called."""
+    _channel_counter = 0  # Class-level counter to assign unique channels to each new instance
 
     def __init__(self):
-        """
-        Class for creating and managing pygame channel and sound objects.
-        Keeps track of channels used, assigns new
-        channel each time class is called.
-        Allows for loading up multiple audio files for a channel,
-        which avoids the lag of loading files repeatedly.
-        Also allows for ignoring repeat requests to
-        play a song that is already playing.
-
-        Examples:
-            a = Audio()
-            a.add_audio(audio1_file_path)
-            a.add_audio(audio2_file_path)
-            a.play_audio('audio1', loops=-1)
-            a.play_audio('audio1') # does nothing since audio already playing
-            a.play_audio('audio1', override=True) # replays audio1 regardless
-        """
         self.channel_id = Audio._channel_counter
         Audio._channel_counter += 1
         self.channel = pygame.mixer.Channel(self.channel_id)
@@ -35,33 +21,29 @@ class Audio:
         self.current_audio = None
 
     def add_audio(self, audio_dir: str, tag: str = None) -> None:
-        """
-        Load audio from the given path.
+        """Load audio from the given path
 
-        Args:
-            tag: The name of the audio to associate with the audio directory,
-            if unspecified will use the name of the audio file.
-            audio_dir: Directory of the audio file.
+        :param audio_dir: Directory of the audio file to add.
+        :param tag: The name of the audio to associate with the sound object,
+            defaults to None, in which case the filename will be used as the tag.
         """
         if not os.path.isfile(audio_dir):
             raise FileNotFoundError(f"File {audio_dir} does not exist.")
         tag = Path(audio_dir).stem if tag is None else None
         self.audio_list[tag] = pygame.mixer.Sound(audio_dir)
 
-    def play_audio(self, tag: str = None, loops: int = 0,
-                   override: bool = False) -> None:
-        """
-        Plays the audio of the tag specified.
-        Does nothing if the same audio file is already playing.
+    def play_audio(self, tag: str = None, loops: int = 0, override: bool = False) -> None:
+        """Plays the audio of the tag specified if the channel is not busy.
 
-        Args:
-            tag: The name of the audio to play, specified when using add_audio.
-            Can be left unspecified if only one audio is loaded into the class.
-            loops: Amount of times to loop the audio, -1 for looping
-            indefinately.
-            override: Whether the audio should play even if the currently
-            playing audio is equal to the audio being requested to play.
-        """
+        :param tag: The name of the audio to play, defaults to None which will use the first
+            audio file added to that channel (this should only be used if there is only one
+            audio file loaded for clarity).
+        :param loops: Amount of times to loop the audio, -1 loops indefinately.
+        :param override: Whether the audio should play even if the channel is busy. 
+            The previously playing audio will stop.
+
+        :raises KeyError: If no audio has been added to the channel.
+        :raises ValueError: If the loops argument is smaller than -1."""
         if len(self.audio_list) == 0:
             raise KeyError("No audio loaded to play.")
         elif len(self.audio_list) > 1 and tag is None:
@@ -69,9 +51,7 @@ class Audio:
         if loops < -1:
             raise ValueError("loops argument cannot be smaller than -1.")
 
-        # set tag to first sound object if unspecified
         tag = self.audio_list[0] if tag is None else tag
-        # force stop the currently playing audio if override is True
         self.stop() if override else None
         if not self.channel.get_busy():
             self.current_audio = None
@@ -79,13 +59,15 @@ class Audio:
             self.channel.play(self.audio_list[tag], loops=loops)
             self.current_audio = tag
 
-    def stop(self):
+    def stop(self) -> None:
         """Stop the currently playing audio."""
         self.channel.stop()
 
-    def set_volume(self, volume: float):
-        """Set the volume of the audio. Volume should be between 0.0 and 1.0.
-        """
+    def set_volume(self, volume: float) -> None:
+        """Set the volume of the audio.
+        
+        :param volume: The volume to set the channel to.
+        :raises ValueError: If the volume is not between 0.0 or 1.0"""
         if not 0.0 <= volume <= 1.0:
             raise ValueError("Volume must be between 0.0 and 1.0.")
         self.channel.set_volume(volume)
