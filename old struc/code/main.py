8615a25e4ca447964a610af1890841e9e0863a58
r5ne <renetimantsev@gmail.com>
2024-11-04 19:48:34 +0000

    Preparing for merging branch into main. Deleted old file structure since most features have been implemented and old files don't need to be referenced anymore.

--- old struc/code/main.py
+++
@@ -1,9 +0,0 @@
-from . import tools
-
-def main():
-    run = tools.Control()
-    state_dict = {
-
-    }
-    run.setup_states(state_dict)
-    run.main()
--- old struc/code/states/title1.py
+++
@@ -1,7 +0,0 @@
-from .. import tools
-
-
-class Title(tools._State):
-    def __init__(self):
-        tools._State.__init__(self)
-        self.next = "Options"
