c68e0792d4b37b24ded12ed5ace0d12fb4f70dbd
r5ne <renetimantsev@gmail.com>
2024-11-05 23:52:33 +0000

    Finished making popup.py alignment for text and buttons. Now works, yet to implement any functionality

--- data/components/ui/popup.py
+++ data/components/ui/popup.py
@@ -1,14 +1,12 @@
-import logging
 from dataclasses import dataclass
 from typing import Optional, override
-
 import pygame
 from pygame import freetype
-
 from data.components import RectAlignments
 from data.components.ui import ButtonBase, Text
 from data.components.ui.widgetutils import WidgetBase
 from data.core import screen
+from data.core.prepare import screen_center
 from data.core.utils import Popups, ColorPalette
 
 
@@ -22,74 +20,71 @@ class PopupConfig:
     text_color: Optional[pygame.Color | tuple] = None
     font: Optional[pygame.freetype.Font] = None
     font_size: int = 32
-    buttons: tuple[ButtonBase]
-    padding: int = 10
+    buttons: tuple[ButtonBase, ...] = ()
+    padding: int = 0
     popup_type: Popups = 0
-    surface: pygame.Surface = pygame.display.get_surface()
 
 
 class Popup(WidgetBase):
     def __init__(self, config: PopupConfig):
-        position = tuple(round(coord/2) for coord in
-                         pygame.display.get_surface().get_size()) \
-            if config.position is None else config.position
+        position = screen_center if config.position is None else config.position
         super().__init__(position, config.align)
         self._width, self._height = config.size
-        self._color = config.color if config.color is not None else \
-            ColorPalette.PRIMARY
-        self._popup_type = config.popup_type
-        self._buttons = config.buttons
-        total_button_width, total_button_height = self._get_button_sizes()
+        self._color = config.color or ColorPalette.PRIMARY
         self._rect = pygame.Rect(0, 0, self._width, self._height)
         self._align_rect(self._rect, self.align, (self._x, self._y))
-        self._text = Text(
-            (self._rect.centerx, round(self._rect.top+self._rect.height/3)),
-            config.text, config.font, config.font_size, 
-            config.text_color if config.text_color is not None else \
-                pygame.Color('white'),
-            'midtop', sub_widget=True)
-        self._text.x -= self._text.rect.height
-        self.padding = config.padding
-        logging.info(f'Created {repr(self)}.')
+        self._text = None
+        if config.text:
+            self._text = Text(
+                (self._rect.centerx, (self._rect.top + int(self._height * 0.2)) \
+                    if config.buttons else self._rect.centery),
+                config.text, config.font, config.font_size,
+                config.text_color if config.text_color is True else \
+                    pygame.Color('white'),
+                'midtop' if config.buttons else 'center',
+                sub_widget=True)
 
-    def _get_button_sizes(self) -> tuple[int, int]:
-        total_button_width, total_button_height = 0, 0
-        for button in self._buttons:
-            total_button_width += button.width
-            total_button_height += button.height
-            if not button.sub_widget:
-                raise ValueError(f'Button passed: {button} is not a sub '
-                                 f'widget. All buttons should be sub widgets.')
-        if total_button_width >= self._width:
-            raise ValueError(f"Total width of buttons ({total_button_width}) "
-                             f"is more than width of popup given ({self._width})")
-        if total_button_height >= self._height:
-            raise ValueError(f"Total height of buttons ({total_button_height}) "
-                             f"is more than height of popup given ({self._height})")
-        return total_button_width, total_button_height
+        # Buttons setup
+        self._buttons = config.buttons
+        if self._buttons:
+            self._align_buttons(self._buttons, self._rect, config.padding)
 
     def _align_rect(self, rect, align, coords):
         setattr(rect, align, coords)
         self._x, self._y = getattr(rect, align)
 
-    def _align_buttons(self, buttons: tuple[ButtonBase], rect: pygame.Rect):
-        num_of_buttons = len(buttons)
-        left = rect.left
-        for index, button in enumerate(buttons):
-            padding = (rect.width - num_of_buttons * button.width) / num_of_buttons + 1
-            button.x = left + padding * index
-            left = button.x + button.width
-            button.y = self._rect.bottom - round(self._rect.height/3) - button.height
+    def _align_buttons(self, buttons: tuple[ButtonBase, ...], rect: pygame.Rect, padding: int):
+        # Calculate button positioning based on rect
+        total_button_width = sum(button.width for button in buttons)
+        gaps = len(buttons) + 1
+        button_padding_x = round((rect.width - total_button_width) / gaps)
+
+        # Position each button within the rect
+        y_position = rect.centery if not self._text else rect.bottom - int(self._height * 0.1)
+        print(y_position, rect.bottom)
+        left = rect.left + button_padding_x
+        for button in buttons:
+            button.align = 'bottomleft' if self._text else 'midleft'
+            button.x = left
+            button.y = y_position
+            left += button.width + button_padding_x
 
     @override
-    def update(self) -> None:
-        self._text.update()
+    def update(self):
+        if self._text:
+            self._text.update()
         for button in self._buttons:
             button.update()
 
     @override
-    def blit(self) -> None:
+    def blit(self):
+        # Draw background
         pygame.draw.rect(screen, self._color, self._rect)
-        self._text.blit()
+
+        # Draw text if available
+        if self._text:
+            self._text.blit()
+
+        # Draw each button
         for button in self._buttons:
             button.blit()
--- data/states/options.py
+++ data/states/options.py
@@ -1,15 +1,34 @@
-from .state import State
+from data.components.ui import Popup, PopupConfig, TextButton, TextButtonConfig
+from data.core import Colors
+from data.core.prepare import screen_center
+from data.states.state import State
 
 
 class Options(State):
     def __init__(self):
         super().__init__()
+        text_btn_config = TextButtonConfig(position=screen_center, size=(100, 30),
+                                           text='click', align='midleft',
+                                           colors={
+                                               'default': Colors.BACKGROUND,
+                                               'hovered': Colors.FOREGROUND,
+                                               'clicked': Colors.ACCENT
+                                           })
+        x= TextButton(text_btn_config)
+        text_btn_config.text = 'bye'
+        x1=TextButton(text_btn_config)
+        text_btn_config.text = '~'
+        x2=TextButton(text_btn_config)
+        popup_config = PopupConfig(position=screen_center, size=(400, 100), buttons=(x,x1, x2), text='HELLO BRO',
+                                   padding=20, color=(14, 14, 14))
+        self.popup = Popup(popup_config)
+        self.widgets = (self.popup,)
 
     def update_screen(self):
         ...
 
     def update(self):
-        ...
+        super().update()
 
     def render(self):
         super().render()
