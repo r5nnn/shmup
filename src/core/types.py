4f1cc2b1a39decbbd1dfbb970df36f3f1d4db61e
r5ne <renetimantsev@gmail.com>
2025-03-07 20:45:23 +0000

    Moved all the game's type hint variables to one file.
    Some files still rely on old directories. This will be fixed in future commits.

---
+++ src/core/types.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from typing import Literal
+
+import pygame
+
+from src.components.ui.buttons import (
+    TextToggleButton, TextClickButton, TextRectToggleButton,
+    TextRectClickButton, ImageClickButton, ImageToggleButton,
+    ImageRectToggleButton, ImageRectClickButton)
+from src.states import Title, Options, Game
+
+# Literals
+
+RectAlignments = Literal[
+    "topleft", "midtop", "topright",
+    "midleft", "center", "midright",
+    "bottomleft", "midbottom", "bottomright"]
+TextRectAlignments = Literal["right", "left", "center", "justified"]
+EventTypes = Literal[
+    "key", "keydown", "keyup",
+    "mouse", "mousedown", "mouseup",
+    "quit",
+]
+
+# Complex
+
+Colors = list[tuple | pygame.Color] | tuple | pygame.Color | None
+Align = list[Literal["top", "bottom", "left", "right"]] | Literal["center"]
+Images = tuple[pygame.Surface, ...] | pygame.Surface | str
+
+# Any
+
+AnyButton = (TextToggleButton | TextClickButton | TextRectToggleButton
+            | TextRectClickButton | ImageClickButton | ImageToggleButton
+            | ImageRectClickButton | ImageRectToggleButton)
+AnyState = Title | Options | Game
