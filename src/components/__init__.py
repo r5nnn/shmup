024119d2f94b8053cc40014b5980475ca9aa7611
r5ne <renetimantsev@gmail.com>
2025-03-07 20:48:35 +0000

    Removed accidental ui widget imports placed in components package rather than the ui package.

--- src/components/__init__.py
+++ src/components/__init__.py
@@ -1,21 +1,3 @@
 """Package containing components that help run the game."""
 from src.components.audio import Audio, background_audio, button_audio
 from src.components.colors import shift_rgb, ColorGradient
-from src.components.ui.text import Text, TextArray
-from src.components.ui.buttons.imagebutton import (
-    ImageToggleButton, ImageClickButton, ImageRectClickButton,
-    ImageRectToggleButton, ImageButtonConfig)
-from src.components.ui.buttons.textbutton import (
-    TextClickButton, TextToggleButton, TextRectClickButton,
-    TextRectToggleButton, TextButtonConfig)
-from src.components.ui.buttons.textbuttonarray import (
-    TextToggleButtonArray, TextToggleButtonArrayConfig, TextClickButtonArray,
-    TextClickButtonArrayConfig, TextRectToggleButtonArray,
-    TextRectToggleButtonArrayConfig, TextRectClickButtonArray,
-    TextRectClickButtonArrayConfig)
-from src.components.ui.buttons.imagebuttonarray import (
-    ImageToggleButtonArray, ImageToggleButtonArrayConfig,
-    ImageClickButtonArray,
-    ImageClickButtonArrayConfig)
-from src.components.ui.buttons.togglegroup import (
-    ToggleButtonGroup, ToggleArrayGroup)
