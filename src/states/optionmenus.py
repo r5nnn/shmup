012dbc72c7b3109942cf817b569de7bc94b54eeb
r5ne <renetimantsev@gmail.com>
2025-03-09 11:56:06 +0000

    Implemented text array for option overlays. Reverted button array to buttons for better control of alignment. Added back all the buttons.
    Stopped using button arrays for the toggle buttons in the options overlays, since the buttons only repeat like twice, and aligning them with text is impossible using only a set offset for each one.

--- src/states/optionmenus.py
+++ src/states/optionmenus.py
@@ -1,65 +1,61 @@
+"""Collection of overlays used by the button state."""
 import pygame
 
-from src.components.ui import Text
-from src.core import toggle_fullscreen
-from src.core.constants import PRIMARY, SECONDARY, ACCENT
-from src.core.utils import toggle_flag
-from src.states.state import Overlay
-from src.core.data import config
 from src.components.managers import statemanager
+from src.components.ui import (
+    TextArray,
+    TextArrayConfig,
+    TextRectToggleButtonArray,
+    TextRectToggleButtonArrayConfig, TextRectToggleButton, TextButtonConfig
+)
+from src.core import toggle_fullscreen, toggle_flag, config
+from src.states.state import Overlay
 
 
 class GraphicsOptions(Overlay):
     def __init__(self):
         super().__init__()
-        self.fullscreen_text = Text(
+        config_ = TextArrayConfig(
+            (("Fullscreen:", "Borderless:", "Resolution:"),))
+        self.text_row1 = TextArray(
             (statemanager.current_state().bg_rect.left +
              statemanager.current_state().padding,
              statemanager.current_state().bg_rect.top +
              statemanager.current_state().option_headings_group.buttons[0].height +
-             statemanager.current_state().padding),
-            text="Fullscreen:")
-        # fullscreen_config = ToggleableTextButtonConfig(
-        #     position=(self.fullscreen_text.rect.right +
-        #               statemanager.current_state().padding,
-        #               self.fullscreen_text.rect.centery),
-        #     size=(200, 30), colors=(PRIMARY, SECONDARY, ACCENT),
-        #     text=("True", "False"),
-        #     start_text=0 if config["flags"]["fullscreen"] else 1,
-        #     align="midleft", on_toggle=toggle_fullscreen)
-        # self.fullscreen_button = ToggleableTextButton(fullscreen_config)
-        # self.borderless_text = Text(
-        #     (statemanager.current_state().bg_rect.left + statemanager.current_state().padding,
-        #      self.fullscreen_text.rect.bottom + statemanager.current_state().padding),
-        #     text="Borderless:")
-        # borderless_config = ToggleableTextButtonConfig(
-        #     position=(self.borderless_text.rect.right +
-        #               statemanager.current_state().padding,
-        #               self.borderless_text.rect.centery),
-        #     size=(200, 30), colors=(PRIMARY, SECONDARY, ACCENT),
-        #     text=("True", "False"),
-        #     start_text=0 if config["flags"]["noframe"] else 1, align="midleft",
-        #     on_toggle=lambda: toggle_flag(flag=pygame.NOFRAME))
-        # self.borderless_button = ToggleableTextButton(borderless_config)
-        # self.resolution_text = Text(
-        #     (statemanager.current_state().bg_rect.left + statemanager.current_state().padding,
-        #      self.borderless_text.rect.bottom + statemanager.current_state().padding),
-        #     text="Resolution:")
-        self.widgets = (self.fullscreen_text, )
+             statemanager.current_state().padding * 1.5), (3, 1),
+            statemanager.current_state().padding * 1.5, config_)
+        button_size = (200, self.text_row1.texts[0].rect.height + 10)
+        button_text = ["False", "True"]
+        config_ = TextButtonConfig(
+            position=(self.text_row1.texts[0].rect.right
+                      + statemanager.current_state().padding,
+                      self.text_row1.texts[0].rect.centery), align="midleft")
+        self.fullscreen_button = TextRectToggleButton(
+            config_, button_size, button_text,
+            start_text=0 if config["flags"]["fullscreen"] else 1,
+            on_toggle_on=toggle_fullscreen, on_toggle_off=toggle_fullscreen)
+        config_.position = (self.text_row1.texts[1].rect.right
+                            + statemanager.current_state().padding,
+                            self.text_row1.texts[1].rect.centery)
+        self.borderless_button = TextRectToggleButton(
+            config_, button_size, button_text,
+            on_toggle_on=lambda: toggle_flag(pygame.NOFRAME),
+            on_toggle_off=lambda: toggle_flag(pygame.NOFRAME))
+        self.widgets = (self.text_row1, self.fullscreen_button,
+                        self.borderless_button)
 
     def render(self) -> None:
         super().render()
 
     def update(self, *args) -> None:
-        # if config["flags"]["fullscreen"]:
-        #     self.fullscreen_button.toggle_on()
-        # else:
-        #     self.fullscreen_button.toggle_off()
-        # if config["flags"]["noframe"]:
-        #     self.borderless_button.toggle_on()
-        # else:
-        #     self.borderless_button.toggle_off()
-        ...
+        if config["flags"]["fullscreen"]:
+            self.fullscreen_button.toggle_on()
+        else:
+            self.fullscreen_button.toggle_off()
+        if config["flags"]["noframe"]:
+            self.borderless_button.toggle_on()
+        else:
+            self.borderless_button.toggle_off()
 
 
 class KeybindsOptions(Overlay):
