716afbf93cf564251f0ebc805c6034bdc5ea9c9c
r5ne <renetimantsev@gmail.com>
2025-03-21 00:22:23 +0000

    Reduced redundant code in state.py

--- src/states/state.py
+++ src/states/state.py
@@ -6,58 +6,45 @@ from typing import override
 
 import pygame
 
-from src.components import events
-from src.core.data import system_data
 from src.components.managers import statemanager, overlaymanager
 from src.components.ui import widgethandler
+from src.core import Printable
+from src.core.data import system_data
 
 
-class State:
+class Overlay(Printable):
     def __init__(self):
-        if len(statemanager.state_stack) >= 1:
-            self.background = statemanager.current_state().background
-        else:
-            self.background = pygame.Surface(system_data.abs_window_rect.size)
+        self.background = None
         self.widgets = []
 
     def startup(self) -> None:
-        for widget in self.widgets:
-            widgethandler.add_widget(widget)
+        widgethandler.add_widget(*self.widgets)
 
     def cleanup(self) -> None:
-        for widget in self.widgets:
-            widgethandler.remove_widget(widget)
+        widgethandler.remove_widget(*self.widgets)
 
     def update(self) -> None:
         widgethandler.update()
 
     def render(self) -> None:
-        system_data.abs_window.blit(self.background, (0, 0))
         widgethandler.blit()
 
-    def back(self) -> None:
-        statemanager.pop()
-
-
-class Overlay(State):
-    def __init__(self):
-        super().__init__()
-        self.background = None
-
-    @override
-    def render(self) -> None:
-        widgethandler.blit()
-
-    @override
-    def startup(self) -> None:
-        for widget in self.widgets:
-            widgethandler.add_widget(widget)
-
-    @override
-    def cleanup(self) -> None:
-        for widget in self.widgets:
-            widgethandler.remove_widget(widget)
-
-    @override
     def back(self) -> None:
         overlaymanager.pop()
+
+
+class State(Overlay):
+    def __init__(self):
+        super().__init__()
+        if len(statemanager.state_stack) >= 1:
+            self.background = statemanager.current_state().background
+        else:
+            self.background = pygame.Surface(system_data.abs_window_rect.size)
+
+    def render(self) -> None:
+        system_data.abs_window.blit(self.background, (0, 0))
+        super().render()
+
+    @override
+    def back(self) -> None:
+        statemanager.pop()
