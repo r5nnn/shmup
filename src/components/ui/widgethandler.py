285b2ded0b5ba68b030edef8f6b26300fa170430
r5ne <renetimantsev@gmail.com>
2025-03-25 22:14:41 +0000

    Reverted the widgethandler overhall. Only top level widgets are part of the widgethandler ordered weakset.
    Added logic for handling composite widget overlapping. Contains of a composite widget returns a list of all subwidgets of that widget that have collided with the cursor (or self if all subwidgets have collided). The update method of the composite widget is then called with all those collided subwidgets if blocked is true, and the composite widget handles not updating those widgets. Non composite widgets use old logic.
    
    Reverted move_to_top and move_to_bottom being made compatible with multiple widgets. Usually only called with one widget, and for loops create enough overhead to warrant removal.

--- src/components/ui/widgethandler.py
+++ src/components/ui/widgethandler.py
@@ -26,21 +26,47 @@ def blit() -> None:
 
 
 def update() -> None:
+    pos = events.get_mouse_pos()
     blocked = False
+
     for widget in reversed(list(widgets)):
-        if not widget.disabled:
-            if not blocked or not widget.contains(*events.get_mouse_pos()):
+        if widget.disabled:
+            continue  # Skip disabled widgets immediately
+
+        # Composite widget: it has children that need special handling
+        if widget.has_sub_widgets:
+            if not (interacted_subwidgets := widget.contains(*pos)):
+                # No subwidgets are being interacted with so normal update
                 widget.update()
-            # Ensure widgets covered by others are not affected (widgets created later)
-            if widget.contains(*events.get_mouse_pos()):
+            elif not blocked:
+                # First widget interacted with
+                widget.update()
+                if not widget.allows_passthrough:
+                    # Make sure other sprites also interacted with are not updated.
+                    blocked = True
+            else:
+                # Some widget is already blocking -> update only the non-disabled subwidgets
+                widget.update(interacted_subwidgets)
+
+        # Non-composite widget
+        else:
+            if not blocked or not widget.contains(*pos):
+                widget.update()
+            if widget.contains(*pos) and not widget.allows_passthrough:
                 blocked = True
 
 
 def add_widget(*widgets_: WidgetBase) -> None:
     for widget in widgets_:
         if widget not in widgets:
-            _add_widget_with_subwidgets(
-                widget, add_sub_widgets_first=not widget.sub_widget_on_top
+            widgets.add(widget)
+            move_to_top(widget)
+        elif widget.sub_widget:
+            warnings.warn(
+                f"Attempted to add subwidget: {widget!r} to the widgethandler."
+                f" Subwidgets should not be added to the widgethandler as the "
+                f"parent widget renders and updates the subwidget.",
+                stacklevel=2,
             )
         else:
             warnings.warn(
@@ -50,66 +76,38 @@ def add_widget(*widgets_: WidgetBase) -> None:
             )
 
 
-def _add_widget_with_subwidgets(
-    widget: WidgetBase, *, add_sub_widgets_first: bool
-) -> None:
-    logger.debug("Widget: %s's sub_widget_on_top is %s, so adding sub "
-                 "widgets %s", widget.__class__.__name__, widget.sub_widget_on_top,
-                 "last" if add_sub_widgets_first else "first")
-    def add_widget_() -> None:
-        widgets.add(widget)
-        move_to_top(widget)
-
-    if not add_sub_widgets_first:
-        add_widget_()
-    for sub_widget in widget.sub_widgets:
-        _add_widget_with_subwidgets(
-            sub_widget, add_sub_widgets_first=not sub_widget.sub_widget_on_top
-        )
-    if add_sub_widgets_first:
-        add_widget_()
-    logger.info("Added widget %s to widgethandler.", repr(widget))
-
-
 def remove_widget(*widgets_: WidgetBase) -> None:
     try:
         for widget in widgets_:
-            _remove_widget_with_subwidgets(widget)
+            widgets.remove(widget)
     except ValueError as e:
         warnings.warn(
             "Attempted to remove widget which doesn't exist in "
-            f"the widgethandler: {e}",
+            f"the widgethandler: "
+            f"{[widget_.__class__.__name__ for widget_ in list(widgets)]} "
+            f"Error: {e}",
             stacklevel=2,
         )
 
 
-def _remove_widget_with_subwidgets(widget: WidgetBase) -> None:
-    for sub_widget in widget.sub_widgets:
-        _remove_widget_with_subwidgets(sub_widget)
-    widgets.remove(widget)
-    logger.info("Removed widget %s from the widgethandler.", repr(widget))
-
-
-def move_to_top(*widgets_: WidgetBase) -> None:
+def move_to_top(widget: WidgetBase) -> None:
     try:
-        for widget in widgets_:
-            widgets.move_to_end(widget)
-    except KeyError as e:
+        widgets.move_to_end(widget)
+    except KeyError:
         warnings.warn(
-            f"Attempted to move widget to the top when"
-            f" widget doesn't exist in widgethandler: {e}.",
+            f"Attempted to move widget: {widget!r} to the top when"
+            f" widget doesn't exist in widgethandler.",
             stacklevel=2,
         )
 
 
-def move_to_bottom(*widgets_: WidgetBase) -> None:
+def move_to_bottom(widget: WidgetBase) -> None:
     try:
-        for widget in widgets_:
-            widgets.move_to_start(widget)
-    except KeyError as e:
+        widgets.move_to_start(widget)
+    except KeyError:
         warnings.warn(
-            f"Tried to move widget to bottom when widget "
-            f"doesn't exist in widgethandler: {e}",
+            f"Error: Tried to move {widget!r} to bottom when {widget!r} "
+            f"doesn't exist in widgethandler.",
             stacklevel=2,
         )
 
